caaf82e225d3621819f99a6be1f15756536d9e39
AlexVipond <alex.r.vipond@gmail.com>
2022-01-12 23:23:19 -0600

    upgrade prepare

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -1,18 +1,4 @@
-import { rollup } from 'rollup'
 import configs from '../rollup.config'
+import { prepare } from '@baleada/prepare'
 
-async function prepare () {
-  for (const config of configs) {
-    const bundle = await rollup(config)
-
-    if (!Array.isArray(config.output)) {
-      throw new Error(`config should be an array`)
-    }
-
-    for (const output of config.output) {
-      await bundle.write(output)
-    }
-  }
-}
-
-prepare()
+prepare(configs)
